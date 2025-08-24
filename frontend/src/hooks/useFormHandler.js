0bfc0977531bcc569e4b361e64bd9ef468c08f66
thulebonagwala <thulebonagwala@gmail.com>
2025-08-24 23:50:36 +0200

    Create useFormHandler.js

---
+++ frontend/src/hooks/useFormHandler.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const [forms, setForms] = useState({});
+
+// const handleChange = (e) => {
+
+//     const { name, value } = target.e
+
+//     setForms({ ...forms, [name]: value });
+// };
+
+// const handleChange = (formName) => {
+//     (e) => {
+
+//         const { name, value } = target.e
+
+//         setForms({ ...forms, [formName]: { ...forms[formName], [name]: value } })
+//     }
+// }
+
+// const handleChange = (formName) => ({ target: { name, value } }) => 
+//     setForms({ ...forms, [formName]: { ...forms[formName], [name]: value } })
+
+const handleChange = (formName) => ({ target: { name, value } }) =>
+    setForms((prev) =>({
+        ...prev, 
+        [formName]: { ...prev[formName], [name]: value }
+    }))
+
+
+export function useFormHandler(initialForms){
+    const [forms, setForms] = useState({initialForms});
+
+    const handleChange = (formName) => ({ target: { name, value } }) =>
+    setForms((prev) =>({
+        ...prev, 
+        [formName]: { ...prev[formName], [name]: value }
+    }));
+
+    return(forms,handleChange);
+}
